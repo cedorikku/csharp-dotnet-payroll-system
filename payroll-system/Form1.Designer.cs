25359c0c36f22045a19e9b6ce820c0e3e3cc2b7e
Cedric <75742716+cedorikku@users.noreply.github.com>
2023-09-10 15:23:53 +0800

    Add and customize first form as welcome screen

--- payroll-system/Form1.Designer.cs
+++ payroll-system/Form1.Designer.cs
@@ -28,13 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtFirstName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtFirstName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstName.Location = new System.Drawing.Point(10, 10);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(342, 22);
+            this.txtFirstName.TabIndex = 0;
+            this.txtFirstName.Enter += new System.EventHandler(this.txtFirstName_Enter);
+            this.txtFirstName.Leave += new System.EventHandler(this.txtFirstName_Leave);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.txtFirstName);
+            this.panel1.Location = new System.Drawing.Point(68, 93);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(10, 10, 10, 5);
+            this.panel1.Size = new System.Drawing.Size(364, 44);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.txtLastName);
+            this.panel2.Location = new System.Drawing.Point(68, 160);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(10, 10, 10, 5);
+            this.panel2.Size = new System.Drawing.Size(364, 44);
+            this.panel2.TabIndex = 2;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtLastName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLastName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastName.Location = new System.Drawing.Point(10, 10);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(342, 22);
+            this.txtLastName.TabIndex = 0;
+            this.txtLastName.Enter += new System.EventHandler(this.txtLastName_Enter);
+            this.txtLastName.Leave += new System.EventHandler(this.txtLastName_Leave);
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblHeading.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeading.Location = new System.Drawing.Point(0, 0);
+            this.lblHeading.Margin = new System.Windows.Forms.Padding(0);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Padding = new System.Windows.Forms.Padding(60, 20, 0, 0);
+            this.lblHeading.Size = new System.Drawing.Size(204, 60);
+            this.lblHeading.TabIndex = 0;
+            this.lblHeading.Text = "Welcome";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.Black;
+            this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSubmit.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F);
+            this.btnSubmit.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnSubmit.Location = new System.Drawing.Point(68, 231);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(364, 44);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "Get Started";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(71, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(71, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Last Name";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(549, 473);
+            this.ClientSize = new System.Drawing.Size(504, 312);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblHeading);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
@@ -42,11 +150,25 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Payroll System";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- payroll-system/Form1.cs
+++ payroll-system/Form1.cs
@@ -15,6 +15,40 @@ namespace payroll_system
         public Form1()
         {
             InitializeComponent();
+            txtFirstName.Text = "First Name";
+            txtLastName.Text = "Last Name";
+        }
+
+        private void txtFirstName_Enter(object sender, EventArgs e)
+        {
+            if (txtFirstName.Text == "First Name")
+            {
+                txtFirstName.Text = "";
+            }
+        }
+
+        private void txtFirstName_Leave(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtFirstName.Text))
+            {
+                txtFirstName.Text = "First Name";
+            }
+        }
+
+        private void txtLastName_Enter(object sender, EventArgs e)
+        {
+            if (txtLastName.Text == "Last Name")
+            {
+                txtLastName.Text = "";
+            }
+        }
+
+        private void txtLastName_Leave(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtLastName.Text))
+            {
+                txtLastName.Text = "Last Name";
+            }
         }
     }
 }
