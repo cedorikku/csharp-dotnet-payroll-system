64a41e8864fcea12767dc7f7ea3caeac22033974
Cedric <75742716+cedorikku@users.noreply.github.com>
2023-09-10 16:19:48 +0800

    Add class in library for storing user information

---
+++ DataHelper/UserInformation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataHelper
+{
+    public class UserInformation
+    {
+        private string firstname, lastname;
+
+        public string Firstname
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+        public string Lastname
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+    }
+}
