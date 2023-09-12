9d039f494cd8b2fb6017f8e705797701fce71e8d
Cedric <75742716+cedorikku@users.noreply.github.com>
2023-09-12 22:47:48 +0800

    Set this form to non-resizable

--- payroll-system/Form2.Designer.cs
+++ payroll-system/Form2.Designer.cs
@@ -528,6 +528,7 @@
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox2);
             this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "Form2";
